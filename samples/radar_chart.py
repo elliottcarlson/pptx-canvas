ad50cbd6ccebfa1bc5c1e06382c214c97ffb43ef
Elliott Carlson <x@sublim.nl>
2016-04-24 01:33:36 -0400

    add font styling to strokeText

--- samples/radar_chart.py
+++ samples/radar_chart.py
@@ -28,8 +28,6 @@ class Radar(object):
             return 3
         if x >= 0 and y <= 0:
             return 4
-        print x
-        print y
         raise Exception
 
     def getVertices(self, x, y):
